284f105d3656afe85fda7eda6df125e7f79c8345
juexro <juexro@163.com>
2024-08-16 17:30:44 +0800

    feat: autocompletion extension

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,12 @@
 import { parser } from "./st.grammar";
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
+import { Extension } from "@codemirror/state";
+import { Completion } from "@codemirror/autocomplete";
 declare const iecstLanguage: LRLanguage;
 declare function iecst(): LanguageSupport;
-export { iecstLanguage, iecst, parser };
+interface ICompletion extends Completion {
+    children?: ICompletion[];
+    alias?: string | string[];
+}
+declare function autocomplete(data?: ICompletion[]): Extension;
+export { iecstLanguage, iecst, parser, ICompletion, autocomplete };
